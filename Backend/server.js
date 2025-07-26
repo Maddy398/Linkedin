69411c6c0819dfb41907e96b260ff5ad644df891
Maddy398 <madhupsharma398@gmail.com>
2025-07-26 17:42:19 +0530

    last one

--- Backend/server.js
+++ Backend/server.js
@@ -43,7 +43,7 @@ mongoose
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:3000`);
 });
